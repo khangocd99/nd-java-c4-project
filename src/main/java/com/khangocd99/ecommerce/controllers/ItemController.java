cde8b1c176e86d1487fbc366d22d16669fc8186b
khangocd99 <khangocd99@gmail.com>
2023-11-19 16:39:56 +0700

    Configure log4j2

--- src/main/java/com/khangocd99/ecommerce/controllers/ItemController.java
+++ src/main/java/com/khangocd99/ecommerce/controllers/ItemController.java
@@ -3,6 +3,8 @@ package com.khangocd99.ecommerce.controllers;
 import java.util.List;
 
 import com.khangocd99.ecommerce.model.persistence.Item;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +18,16 @@ import com.khangocd99.ecommerce.model.persistence.repositories.ItemRepository;
 @RequestMapping("/api/item")
 public class ItemController {
 
+	private static final Logger logger = LogManager.getLogger(ItemController.class);
+
 	@Autowired
 	private ItemRepository itemRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Item>> getItems() {
+		logger.info("Info level log message");
+		logger.debug("Debug level log message");
+		logger.error("Error level log message");
 		return ResponseEntity.ok(itemRepository.findAll());
 	}
 	
