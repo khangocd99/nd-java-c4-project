000f74da2686dacbbe5e63dd0d089b4b728c0060
khangocd99 <khangocd99@gmail.com>
2023-11-19 23:58:09 +0700

    Update logging

--- src/main/java/com/khangocd99/ecommerce/security/JWTAuthenticationFilter.java
+++ src/main/java/com/khangocd99/ecommerce/security/JWTAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.khangocd99.ecommerce.security;
 import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khangocd99.ecommerce.model.persistence.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,6 +23,8 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private static final Logger logger = LogManager.getLogger(JWTAuthenticationFilter.class);
+
     private final AuthenticationManager authenticationManager;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -33,7 +37,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         try {
             User credentials = new ObjectMapper()
                     .readValue(req.getInputStream(), User.class);
-
+            logger.info("Authenticating for user: " + credentials.getUsername());
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             credentials.getUsername(),
@@ -55,5 +59,12 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
                 .sign(HMAC512(SecurityConstants.SECRET.getBytes()));
         res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+        logger.info("Successful authentication");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        logger.info("Unsuccessful authentication");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
